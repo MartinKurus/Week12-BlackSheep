1880066aab7b646c39d54f64f4d1cd3970626d35
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:18:13 +0200

    first commit

---
+++ RandomQuotes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace RandomQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\samples\quotes.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            Console.WriteLine(dataFromFile[0]);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, dataFromFile.Length);
+            Console.WriteLine(dataFromFile[randomIndex]);
+        }
+    }
+}
---
+++ Week12-BlackSheep-File/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Week12_BlackSheep_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\samples\blacksheep.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            for(int i =0; i <dataFromFile.Length; i++ )
+            {
+                dataFromFile[i] = dataFromFile[i].Replace('4', 'a');
+                dataFromFile[i] = dataFromFile[i].Replace('3', 'e');
+                dataFromFile[i] = dataFromFile[i].Replace('0', 'o');
+                dataFromFile[i] = dataFromFile[i].Replace('1', 'i');
+            }
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+
+            //uptatee the file content
+            File.WriteAllLines(filePath, dataFromFile);
+            
+
+
+
+
+        }
+    }
+}
